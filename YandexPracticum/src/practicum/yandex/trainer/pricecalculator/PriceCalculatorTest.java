84587f96f89075e0de8b597f9f72c3ad2a4d2b6d
LionHeartRichard <leonhearthrich@gmail.com>
2024-11-05 11:54:26 +0300

    price-calculator - example test coverage

--- YandexPracticum/src/practicum/yandex/trainer/pricecalculator/PriceCalculatorTest.java
+++ YandexPracticum/src/practicum/yandex/trainer/pricecalculator/PriceCalculatorTest.java
@@ -12,4 +12,49 @@ class PriceCalculatorTest {
 		Assertions.assertTrue(priceCalculator.calculatePrice(TransportType.BIKE, 0) < 0);
 	}
 
+	@Test
+	public void shouldBeNegativeWhenBikeAndDistanceIs21Km() {
+		Assertions.assertTrue(priceCalculator.calculatePrice(TransportType.BIKE, 21) < 0);
+	}
+
+	@Test
+	public void checkPriceDeliveriesBikeWhenDistance10Km() {
+		int expected = 10 * 10;
+		int actual = priceCalculator.calculatePrice(TransportType.BIKE, 10);
+		Assertions.assertEquals(expected, actual);
+	}
+
+	@Test
+	public void shouldBeNegativeWhenCarAndDistanceIs0Km() {
+		Assertions.assertTrue(priceCalculator.calculatePrice(TransportType.CAR, 0) < 0);
+	}
+
+	@Test
+	public void shouldBeNegativeWhenCarAndDistanceIs1001Km() {
+		Assertions.assertTrue(priceCalculator.calculatePrice(TransportType.CAR, 1001) < 0);
+	}
+
+	@Test
+	public void checkPriceDeliveriesCarWhenDistance700Km() {
+		int expected = 7 * 700;
+		int actual = priceCalculator.calculatePrice(TransportType.CAR, 700);
+		Assertions.assertEquals(expected, actual);
+	}
+
+	@Test
+	public void shouldBeNegativeWhenTruckAndDistanceIs0Km() {
+		Assertions.assertTrue(priceCalculator.calculatePrice(TransportType.TRUCK, 0) < 0);
+	}
+
+	@Test
+	public void checkPriceDeliveriesTruckWhenDistance_7_000Km() {
+		int expected = 5 * 7_000;
+		int actual = priceCalculator.calculatePrice(TransportType.TRUCK, 7_000);
+		Assertions.assertEquals(expected, actual);
+	}
+
+	@Test
+	public void checkUsingTransportDroneShouldReturnNull() {
+		Assertions.assertNull(priceCalculator.calculatePrice(TransportType.DRONE, 10));
+	}
 }
