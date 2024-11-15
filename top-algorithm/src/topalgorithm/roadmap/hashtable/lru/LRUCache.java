b6cf6b30b39a3cc635435b4b80019d61744154d7
LionHeartRichard <leonhearthrich@gmail.com>
2024-11-15 21:11:11 +0300

    LRU...

--- top-algorithm/src/topalgorithm/roadmap/hashtable/lru/LRUCache.java
+++ top-algorithm/src/topalgorithm/roadmap/hashtable/lru/LRUCache.java
@@ -3,59 +3,67 @@ package topalgorithm.roadmap.hashtable.lru;
 import java.util.LinkedList;
 import java.util.Map;
 import java.util.Optional;
+import java.util.concurrent.locks.ReentrantReadWriteLock;
 
 public class LRUCache<K, V> implements Cache<K, V> {
 
 	private int size;
-	private Map<K, LinkedListNode<CacheElement<K, V>>> linkedListNodeMap;
-	private DoublyLinkedList<CacheElement<K, V>> linkedList;
+	private final Map<K, LinkedListNode<CacheElement<K, V>>> linkedListNodeMap;
+	private final DoublyLinkedList<CacheElement<K, V>> doublyLinkedList;
+	private final ReentrantReadWriteLock lock = new ReentrantReadWriteLock();
 
-	@Override
+	public LRUCache(int size) {
+		this.size = size;
+		this.linkedListNodeMap = new ConcurrentHashMap<>(size);
+		this.doublyLinkedList = new DoublyLinkedList<>();
+	}
+
 	public boolean put(K key, V value) {
-		CacheElement<K, V> item = new CacheElement<K, V>(key, value);
-		LinkedListNode<CacheElement<K, V>> newNode;
-		if (this.linkedListNodeMap.containsKey(key)) {
-			LinkedListNode<CacheElement<K, V>> node = this.linkedListNodeMap.get(key);
-			newNode = doublyLinkedList.updateAndMoveToFront(node, item);
-		} else {
-			if (this.size() >= this.size) {
-				this.evictElement();
+		this.lock.writeLock().lock();
+		try {
+			CacheElement<K, V> item = new CacheElement<K, V>(key, value);
+			LinkedListNode<CacheElement<K, V>> newNode;
+			if (this.linkedListNodeMap.containsKey(key)) {
+				LinkedListNode<CacheElement<K, V>> node = this.linkedListNodeMap.get(key);
+				newNode = doublyLinkedList.updateAndMoveToFront(node, item);
+			} else {
+				if (this.size() >= this.size) {
+					this.evictElement();
+				}
+				newNode = this.doublyLinkedList.add(item);
 			}
-			newNode = this.doublyLinkedList.add(item);
+			if (newNode.isEmpty()) {
+				return false;
+			}
+			this.linkedListNodeMap.put(key, newNode);
+			return true;
+		} finally {
+			this.lock.writeLock().unlock();
 		}
-		if (newNode.isEmpty()) {
-			return false;
-		}
-		this.linkedListNodeMap.put(key, newNode);
-		return true;
+
 	}
 
-	@Override
 	public Optional<V> get(K key) {
-		LinkedListNode<CacheElement<K, V>> linkedListNode = this.linkedListNodeMap.get(key);
-		if (linkedListNode != null && !linkedListNode.isEmpty()) {
-			linkedListNodeMap.put(key, this.doublyLinkedList.moveToFront(linkedListNode));
-			return Optional.of(linkedListNode.getElement().getValue());
+		this.lock.readLock().lock();
+		try {
+			LinkedListNode<CacheElement<K, V>> linkedListNode = this.linkedListNodeMap.get(key);
+			if (linkedListNode != null && !linkedListNode.isEmpty()) {
+				linkedListNodeMap.put(key, this.doublyLinkedList.moveToFront(linkedListNode));
+				return Optional.of(linkedListNode.getElement().getValue());
+			}
+			return Optional.empty();
+		} finally {
+			this.lock.readLock().unlock();
 		}
-		return Optional.empty();
-	}
-
-	@Override
-	public int size() {
-		// TODO Auto-generated method stub
-		return 0;
-	}
-
-	@Override
-	public boolean isEmpty() {
-		// TODO Auto-generated method stub
-		return false;
-	}
-
-	@Override
-	public void clear() {
-		// TODO Auto-generated method stub
 
 	}
 
+	private boolean evictElement() {
+		this.lock.writeLock().lock();
+		try {
+			// ...
+		} finally {
+			this.lock.writeLock().unlock();
+		}
+	}
 }
