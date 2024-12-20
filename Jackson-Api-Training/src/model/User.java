e1f0847eb33247167b399c6b4c9531d85010a266
LionHeartRichard <leonhearthrich@gmail.com>
2024-12-20 17:40:33 +0300

    Add Jackson Api-new feature!!!

--- Jackson-Api-Training/src/model/User.java
+++ Jackson-Api-Training/src/model/User.java
@@ -1,5 +1,57 @@
 package model;
 
+import java.util.Objects;
+
 public class User {
+	private String id;
+	private String name;
+
+	public User() {
+		super();
+	}
+
+	public User(String id, String name) {
+		super();
+		this.id = id;
+		this.name = name;
+	}
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		User other = (User) obj;
+		return Objects.equals(id, other.id);
+	}
+
+	@Override
+	public String toString() {
+		return "[id=" + id + ", name=" + name + "]";
+	}
 
 }
--- Jackson-Api-Training/src/service/Service.java
+++ Jackson-Api-Training/src/service/Service.java
@@ -1,5 +1,27 @@
 package service;
 
+import model.User;
+import util.Graph;
+
 public class Service {
+
+	protected Graph<User> graph;
+
+	public Service() {
+		super();
+	}
+
+	public Service(Graph<User> graph) {
+		super();
+		this.graph = graph;
+	}
+
+	public Graph<User> getGrap() {
+		return graph;
+	}
+
+	public void setGrap(Graph<User> graph) {
+		this.graph = graph;
+	}
 
 }
