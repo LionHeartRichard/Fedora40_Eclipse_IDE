ad38aebdbce81df4d0f5bb3aef8d5cb23b501138
LionHeartRichard <leonhearthrich@gmail.com>
2024-12-25 12:02:46 +0300

    Add test for Fibonacci

--- MySolutions_LeetCode/src/dinamicprograming/Fibonacci.java
+++ MySolutions_LeetCode/src/dinamicprograming/Fibonacci.java
@@ -1,13 +1,18 @@
 package dinamicprograming;
 
+import static org.junit.Assert.assertEquals;
+
+import org.junit.jupiter.api.Test;
+
 public class Fibonacci {
 
-	private int[] cach = new int[46];
+	private int[] cach = new int[48];
 
 	// Пример динамического программирования снизу!!!!!!!!
 	public int fib(int n) {
 		if (n <= 1)
-			return 1; // return n - used in Example
+			return n; // return n - used in Example or 1 - если нужно исключиьт повторение в начале
+						// 1, 1
 		if (cach[n] == 0)
 			cach[n] = fib(n - 1) + fib(n - 2);
 		return cach[n];
@@ -30,6 +35,38 @@ public class Fibonacci {
 			factorial *= i;
 		}
 		return factorial;
+	}
+
+	@Test
+	public void testFibRecursionWithCach() {
+		int num = 46;
+		int actual = fib(num);
+
+		System.out.println("fib[" + num + "] = " + actual);
+
+		int expected = 1836311903;
+
+		assertEquals(expected, actual);
+	}
+
+	@Test
+	public void testFibDinamic() {
+		int num = 46;
+		int actual = fibDinamic(num);
+
+		System.out.println("fibDinamic[" + num + "] = " + actual);
+
+		int expected = 1836311903;
+
+		assertEquals(expected, actual);
+	}
+
+	@Test
+	public void testFactorial() {
+		long actual = getFactorial(10);
+		long expected = 3628800l;
+
+		assertEquals(expected, actual);
 	}
 
 }
