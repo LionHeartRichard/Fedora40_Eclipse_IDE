28d9c3c121c2d1ef33afc34e4bab265eb4cabfbc
LionHeartRichard <leonhearthrich@gmail.com>
2025-01-29 20:37:38 +0300

    Add in finalTask TEXH-----PROBLEM____!!!

--- Sprint-9_JSON/src/finaltask/Practicum.java
+++ Sprint-9_JSON/src/finaltask/Practicum.java
@@ -1,16 +1,10 @@
 package finaltask;
-
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 import com.google.gson.TypeAdapter;
 import com.google.gson.reflect.TypeToken;
 import com.google.gson.stream.JsonReader;
 import com.google.gson.stream.JsonWriter;
-
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
 
 import java.io.IOException;
 import java.time.LocalTime;
@@ -85,11 +79,11 @@ class SubtitleListTypeToken extends TypeToken<List<SubtitleItem>> {
 }
 
 class LocalTimeTypeAdapter extends TypeAdapter<LocalTime> {
-	private final DateTimeFormatter timeFormatter = DateTimeFormatter.ofPattern("HH:mm:ss.SSSS");
+	private final DateTimeFormatter timeFormatter = DateTimeFormatter.ofPattern("HH:mm:ss.SSS");
 
 	@Override
 	public void write(final JsonWriter jsonWriter, final LocalTime localTime) throws IOException {
-		jsonWriter.value(localTime.format(timeFormatter));
+		jsonWriter.value(timeFormatter.format(localTime));
 	}
 
 	@Override
@@ -102,16 +96,31 @@ public class Practicum {
 
 	public static void main(String[] args) {
 		List<SubtitleItem> subtitles = Arrays.asList(
-				new SubtitleItem(Map.of(SubtitleLanguage.ru, "Здравствуйте!", SubtitleLanguage.en, "Hello!",
-						SubtitleLanguage.cn, "Ni hao"), LocalTime.of(0, 0, 15), LocalTime.of(0, 0, 17)),
-				new SubtitleItem(Map.of(SubtitleLanguage.ru, "Привет!", SubtitleLanguage.en, "Hi!", SubtitleLanguage.cn,
-						"Ni hao"), LocalTime.of(0, 0, 21), LocalTime.of(0, 0, 24)),
-				new SubtitleItem(Map.of(SubtitleLanguage.ru, "Как дела?", SubtitleLanguage.en, "How are you?",
-						SubtitleLanguage.cn, "Ni hao ma"), LocalTime.of(0, 0, 28), LocalTime.of(0, 0, 31)),
-				new SubtitleItem(
-						Map.of(SubtitleLanguage.ru, "Всё хорошо, спасибо!", SubtitleLanguage.en, "I'm fine, thank you!",
-								SubtitleLanguage.cn, "Wo hen hao, xie xie"),
-						LocalTime.of(0, 0, 34), LocalTime.of(0, 0, 37)));
+                new SubtitleItem(Map.of(SubtitleLanguage.ru, "Здравствуйте!",
+                        SubtitleLanguage.en, "Hello!",
+                        SubtitleLanguage.cn, "Ni hao"),
+                        LocalTime.of(0, 0, 15),
+                        LocalTime.of(0, 0, 17)
+                ),
+                new SubtitleItem(Map.of(SubtitleLanguage.ru, "Привет!",
+                        SubtitleLanguage.en, "Hi!",
+                        SubtitleLanguage.cn, "Ni hao"),
+                        LocalTime.of(0, 0, 21),
+                        LocalTime.of(0, 0, 24)
+                ),
+                new SubtitleItem(Map.of(SubtitleLanguage.ru, "Как дела?",
+                        SubtitleLanguage.en, "How are you?",
+                        SubtitleLanguage.cn, "Ni hao ma"),
+                        LocalTime.of(0, 0, 28),
+                        LocalTime.of(0, 0, 31)
+                ),
+                new SubtitleItem(Map.of(SubtitleLanguage.ru, "Всё хорошо, спасибо!",
+                        SubtitleLanguage.en, "I'm fine, thank you!",
+                        SubtitleLanguage.cn, "Wo hen hao, xie xie"),
+                        LocalTime.of(0, 0, 34),
+                        LocalTime.of(0, 0, 37)
+                )
+        );
 
 		// адаптер для преобразования типа LocalTime в String в формате субтитров
 		LocalTimeTypeAdapter localTimeTypeAdapter = new LocalTimeTypeAdapter();
